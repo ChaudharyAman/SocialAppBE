73aa9ae9e01dbc59e8f1c39107de5eacc5438d18
Aman <tomaraman50@gmail.com>
2025-09-22 15:08:23 +0530

    Updated

--- Controllers/userLoginControllere.js
+++ Controllers/userLoginControllere.js
@@ -36,6 +36,8 @@ exports.loginUser = async (req, res) => {
       const options = {
         expires: new Date(Date.now() + 3 * 60 * 60 * 1000),
         httpOnly: true,
+        secure: process.env.NODE_ENV === "production", 
+        sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
       };
 
       return res
