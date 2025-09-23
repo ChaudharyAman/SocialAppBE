6cdf9df47351cd6c22c6bea16ffe3d8dba6502a0
Aman <tomaraman50@gmail.com>
2025-09-23 12:16:56 +0530

    update

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 exports.isAuthenticated = (req, res, next) => {
   try {
-    const token = req?.cookies?.token || req?.headers?.authorization?.split(' ')[1];
+    const token = req?.headers?.authorization?.split(' ')[1] || req?.cookies?.token
 
     if (!token) {
       return res.status(401).json({
